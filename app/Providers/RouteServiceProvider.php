4e091a49bbf520223f3b9a09823ae625bfbe669d
Mindaugas Ažubalis <mindaugas.azubalis@gmail.com>
2017-08-12 14:13:34 +0000

    rest endpoint added. Meeting, registration etc.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -51,8 +51,10 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapWebRoutes()
     {
-        Route::middleware('web')
-             ->namespace($this->namespace)
+        // Route::middleware('web')
+        //      ->namespace($this->namespace)
+        //      ->group(base_path('routes/web.php'));
+        Route::namespace($this->namespace)
              ->group(base_path('routes/web.php'));
     }
 
